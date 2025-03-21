4b528361a9ec919833fae973892f3ac502aafb2a
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-03-21 10:10:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findMaxSum(vector<int>& arr) {
+        // code here
+    
+        return solve(arr);
+        
+    }
+    private:
+    int solve(vector<int>& arr){
+        int n=arr.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return arr[0];
+        
+        int prev2=0;
+        int prev1=0;
+        for(int i=0;i<n;i++){
+            int curr=max(prev1,prev2+arr[i]);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findMaxSum(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
