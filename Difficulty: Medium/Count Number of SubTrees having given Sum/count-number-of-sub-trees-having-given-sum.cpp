76a8cbd517af0a918a764aed9a93884dc7c978f5
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-03-27 22:49:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Number of SubTrees having given Sum/count-number-of-sub-trees-having-given-sum.cpp
@@ -0,0 +1,159 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// Your are required to complete this function
+int countSubtreesWithSumX(Node* root, int x);
+
+int main()
+{
+  int t;
+  cin>>t;
+  getchar();
+  while (t--)
+  {
+     string s;
+     getline(cin,s);
+     Node* root = buildTree(s);
+     
+     int x;
+     cin>>x;
+     getchar();
+     cout << countSubtreesWithSumX(root, x)<<endl;
+  
+cout << "~" << "\n";
+}
+  return 0;
+}
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for C++
+/*
+Structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+//Function to count number of subtrees having sum equal to given sum.
+int subtreeCount = 0;
+
+// Function to calculate the sum of subtrees and count matching subtrees
+int sumg(Node* root, int x) {
+    if (!root) {
+        return 0;
+    }
+
+    // Recursively get the sum of left and right subtrees
+    int leftsum = sumg(root->left, x);
+    int rightsum = sumg(root->right, x);
+
+    // Calculate the total sum of the current subtree
+    int subtreeSum = root->data + leftsum + rightsum;
+
+    // If the subtree sum matches x, increment the count
+    if (subtreeSum == x) {
+        subtreeCount++;  // Use unique global variable
+    }
+
+    // Return the total sum of this subtree
+    return subtreeSum;
+}
+
+// Wrapper function to count subtrees with sum X
+int countSubtreesWithSumX(Node* root, int x) {
+    subtreeCount = 0;  // Reset count before each function call
+    sumg(root, x);
+    return subtreeCount;
+}
