961ac6949c3e9069c4107b3e03c1e78dbb28d0df
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-06-30 13:07:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max min Height/max-min-height.cpp
+++ Difficulty: Hard/Max min Height/max-min-height.cpp
@@ -1,73 +1,45 @@
-//{ Driver Code Starts
-//Initial Template for C++
+class Solution {
+public:
+    bool isOK(int mid, vector<int>& arr, int k, int w) {
+        int n = arr.size();
+        vector<int> diff(n + w + 1, 0); // prefix diff array
+        int ops = 0;
 
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-//User function Template for C++
-
-
-//User function Template for C++
-
-class Solution
-{
-    private:
-    bool isPossible(vector<int>& a, int n, int k, int w, int target) {
-        vector<int> water(n + 1, 0);
-        int currentWater = 0;
-        int remainingK = k;
-    
         for (int i = 0; i < n; i++) {
-            currentWater += water[i];
-            int deficit = target - (a[i] + currentWater);
-            if (deficit > 0) {
-                if (remainingK < deficit) return false;
-                remainingK -= deficit;
-                currentWater += deficit;
-                if (i + w < n) water[i + w] -= deficit;
+            if (i > 0)
+                diff[i] += diff[i - 1]; // accumulate effect
+
+            int curr_height = arr[i] + diff[i];
+
+            if (curr_height < mid) {
+                int add = mid - curr_height;
+                ops += add;
+                if (ops > k) return false;
+
+                diff[i] += add;
+                diff[i + w] -= add; // rollback after window
             }
         }
+
         return true;
     }
-    public:
-        long long int maximizeMinHeight(int n,int k,int w,vector <int> &a)
-        {
-            // code here
-             int low = *min_element(a.begin(), a.end());
-             int high = *max_element(a.begin(), a.end()) + k;
 
-            while (low < high) {
-                int mid = (low + high + 1) / 2;
-                if (isPossible(a, n, k, w, mid)) {
-                    low = mid;
-                } else {
-                    high = mid - 1;
-                }
+    int maxMinHeight(vector<int>& arr, int k, int w) {
+        int low = *min_element(arr.begin(), arr.end());
+        int high = 1e9;
+        int ans = low;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (isOK(mid, arr, k, w)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            return low;
         }
+
+        return ans;
+    }
 };
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,k,w;
-        cin>>n>>k>>w;
-        vector <int> a(n);
-        for(auto &j:a)
-            cin>>j;
-        Solution s;
-        cout<<s.maximizeMinHeight(n,k,w,a)<<endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
