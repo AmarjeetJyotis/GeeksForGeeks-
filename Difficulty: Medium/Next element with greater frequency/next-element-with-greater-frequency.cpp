a196e1939e23ca67ff360e852f81e3dd908ffe52
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-07-08 12:20:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
@@ -1,65 +1,35 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#define N 10000
-using namespace std;
+class Solution {
+  public:
+     vector<int> findGreater(vector<int>& arr) {
 
-
-// } Driver Code Ends
-
-
-class Solution{
-public:
-    vector<int> print_next_greater_freq(int arr[],int n)
-    {
-        vector<int> ans(n,-1);
-        map<int,int> mp;
-        for(int i=0;i<n;i++) mp[arr[i]]++;
-        stack<int> st;
-        int mx=0,ele=-1;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()) ans[i]=-1;
+        unordered_map<int,int>mp;
+        for(auto i:arr){
+            mp[i]++;
+        }
+        vector<int>tempans(arr.size(),-1);
+        stack<int>st;
+        st.push(arr[tempans.size()-1]);
+        for(int i=arr.size()-2;i>=0;i--){
+            if(mp[arr[i]]<mp[st.top()]){
+                tempans[i]=st.top();
+                st.push(arr[i]);
+            }
             else{
                 while(!st.empty()&&mp[st.top()]<=mp[arr[i]]){
                     st.pop();
                 }
-                if(st.empty()) ans[i]=-1;
-                else ans[i]=st.top();
+                if(st.empty()){
+                    tempans[i]=-1; 
+                st.push(arr[i]);
+                }
+                else{
+                     tempans[i]=st.top();
+                st.push(arr[i]);
+                }
             }
-            st.push(arr[i]);
         }
         
-        return ans;
+      return tempans;
     }
+
 };
-
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int arr[N];
-    
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        
-        for(int i=0; i<n; i++)
-            cin>>arr[i];
-        
-        Solution ob;
-        vector<int> ans=ob.print_next_greater_freq(arr,n);
-        for(auto x:ans){
-            cout<<x<<" ";
-        }
-        cout << endl;
-    
-cout << "~" << "\n";
-}
-	return 1;
-}
-
-// } Driver Code Ends
