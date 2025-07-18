7d7d34e6d08e7412703d27187590a9c877e48d9d
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-07-18 13:52:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -1,49 +1,14 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int missingNum(vector<int>& arr) {
         // code here
+        int sum=1;
         sort(arr.begin(), arr.end());
         for(int i=0; i<arr.size(); i++){
-            if(arr[i] != i+1){
-                return i+1;
+            if(sum == arr[i]){
+                sum++;
             }
         }
-        return arr.size()+1;
+        return sum;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.missingNum(a) << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
