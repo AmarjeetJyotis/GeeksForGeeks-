5bc9e8f8829a82f2126b5fd6fb47fa66e01bc920
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-09-17 21:19:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.cpp
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -1,66 +1,33 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-
-
-
 class Solution {
-  public:
+   public:
     string decodedString(string &s) {
-        // code here
+        stack<string> strStack;
+        stack<int> numStack;
         string res = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != ']') {
-                res.push_back(s[i]);
-            }
-            else {
-                string temp = "";
-                while (!res.empty() && res.back() != '[') {
-                    temp.push_back(res.back());
-                    res.pop_back();
-                }
-                reverse(temp.begin(), temp.end());
-                res.pop_back();
-                string num = "";
-                while (!res.empty() && res.back() >= '0' && res.back() <= '9') {
-                    num.push_back(res.back());
-                    res.pop_back();
-                }
-                reverse(num.begin(), num.end());
-                int p = stoi(num);
-                while (p--) {
-                    res.append(temp);
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (isalpha(c)) {
+                res += c;
+            } else if (c == '[') {
+                strStack.push(res);
+                numStack.push(num);
+                res = "";
+                num = 0;
+            } else if (c == ']') {
+                string temp = res;
+                res = strStack.top();
+                strStack.pop();
+                int count = numStack.top();
+                numStack.pop();
+                while (count--) {
+                    res += temp;
                 }
             }
         }
+        
         return res;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-
-        Solution ob;
-        cout << ob.decodedString(s) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
